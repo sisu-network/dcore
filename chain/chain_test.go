d2dde491f69176b052fee64e7b5c914753187460
Benja Heart <visopsys@users.noreply.github.com>
2021-05-13 21:16:15 -0700

    Fix test

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -48,7 +48,7 @@ func newTestChain(name string, config *eth.Config,
 		hasBlock:   make(map[common.Hash]struct{}),
 		blocks:     make([]common.Hash, 0),
 		blkCount:   0,
-		chain:      NewETHChain(config, nil, rawdb.NewMemoryDatabase(), eth.DefaultSettings, true),
+		chain:      NewETHChain(config, nil, rawdb.NewMemoryDatabase(), eth.DefaultSettings, true, nil),
 		outBlockCh: outBlockCh,
 	}
 	tc.insertBlock(tc.chain.GetGenesisBlock())
