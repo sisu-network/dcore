e56915c2b2605d939bcbfb1d0929bbb4ca9e9f99
Billy Pham <visopsys@users.noreply.github.com>
2021-05-27 15:37:18 -0700

    Fix node/api.go

--- node/config.go
+++ node/config.go
@@ -194,6 +194,8 @@ type Config struct {
 
 	// AllowUnprotectedTxs allows non EIP-155 protected transactions to be send over RPC.
 	AllowUnprotectedTxs bool `toml:",omitempty"`
+
+	DcoreVersion string
 }
 
 // IPCEndpoint resolves an IPC endpoint based on a configured value, taking into
