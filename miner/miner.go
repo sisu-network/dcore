9430ded8c639b4320a9f58f305a5881a5d1e1006
Billy Pham <visopsys@users.noreply.github.com>
2021-05-27 21:56:16 -0700

    Fix miner.go

--- miner/miner.go
+++ miner/miner.go
@@ -18,15 +18,12 @@
 package miner
 
 import (
-	"fmt"
 	"math/big"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/eth/downloader"
 	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/sisu-network/dcore/consensus"
 	"github.com/sisu-network/dcore/core"
 	"github.com/sisu-network/dcore/core/types"
@@ -67,93 +64,89 @@ type Miner struct {
 
 func New(eth Backend, config *Config, chainConfig *params.ChainConfig, mux *event.TypeMux, engine consensus.Engine, isLocalBlock func(block *types.Block) bool) *Miner {
 	miner := &Miner{
-		eth:     eth,
-		mux:     mux,
-		engine:  engine,
-		exitCh:  make(chan struct{}),
-		startCh: make(chan common.Address),
-		stopCh:  make(chan struct{}),
-		worker:  newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, true),
+		// eth:     eth,
+		// mux:     mux,
+		// engine:  engine,
+		// exitCh:  make(chan struct{}),
+		// startCh: make(chan common.Address),
+		// stopCh:  make(chan struct{}),
+		worker: newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, true),
 	}
-	go miner.update()
+	// go miner.update()
 
 	return miner
 }
 
-// update keeps track of the downloader events. Please be aware that this is a one shot type of update loop.
-// It's entered once and as soon as `Done` or `Failed` has been broadcasted the events are unregistered and
-// the loop is exited. This to prevent a major security vuln where external parties can DOS you with blocks
-// and halt your mining operation for as long as the DOS continues.
-func (miner *Miner) update() {
-	events := miner.mux.Subscribe(downloader.StartEvent{}, downloader.DoneEvent{}, downloader.FailedEvent{})
-	defer func() {
-		if !events.Closed() {
-			events.Unsubscribe()
-		}
-	}()
+// // update keeps track of the downloader events. Please be aware that this is a one shot type of update loop.
+// // It's entered once and as soon as `Done` or `Failed` has been broadcasted the events are unregistered and
+// // the loop is exited. This to prevent a major security vuln where external parties can DOS you with blocks
+// // and halt your mining operation for as long as the DOS continues.
+// func (miner *Miner) update() {
+// 	events := miner.mux.Subscribe(downloader.StartEvent{}, downloader.DoneEvent{}, downloader.FailedEvent{})
+// 	defer func() {
+// 		if !events.Closed() {
+// 			events.Unsubscribe()
+// 		}
+// 	}()
 
-	shouldStart := false
-	canStart := true
-	dlEventCh := events.Chan()
-	for {
-		select {
-		case ev := <-dlEventCh:
-			if ev == nil {
-				// Unsubscription done, stop listening
-				dlEventCh = nil
-				continue
-			}
-			switch ev.Data.(type) {
-			case downloader.StartEvent:
-				wasMining := miner.Mining()
-				miner.worker.stop()
-				canStart = false
-				if wasMining {
-					// Resume mining after sync was finished
-					shouldStart = true
-					log.Info("Mining aborted due to sync")
-				}
-			case downloader.FailedEvent:
-				canStart = true
-				if shouldStart {
-					miner.SetEtherbase(miner.coinbase)
-					miner.worker.start()
-				}
-			case downloader.DoneEvent:
-				canStart = true
-				if shouldStart {
-					miner.SetEtherbase(miner.coinbase)
-					miner.worker.start()
-				}
-				// Stop reacting to downloader events
-				events.Unsubscribe()
-			}
-		case addr := <-miner.startCh:
-			miner.SetEtherbase(addr)
-			if canStart {
-				miner.worker.start()
-			}
-			shouldStart = true
-		case <-miner.stopCh:
-			shouldStart = false
-			miner.worker.stop()
-		case <-miner.exitCh:
-			miner.worker.close()
-			return
-		}
-	}
-}
+// 	shouldStart := false
+// 	canStart := true
+// 	dlEventCh := events.Chan()
+// 	for {
+// 		select {
+// 		case ev := <-dlEventCh:
+// 			if ev == nil {
+// 				// Unsubscription done, stop listening
+// 				dlEventCh = nil
+// 				continue
+// 			}
+// 			switch ev.Data.(type) {
+// 			case downloader.StartEvent:
+// 				wasMining := miner.Mining()
+// 				miner.worker.stop()
+// 				canStart = false
+// 				if wasMining {
+// 					// Resume mining after sync was finished
+// 					shouldStart = true
+// 					log.Info("Mining aborted due to sync")
+// 				}
+// 			case downloader.FailedEvent:
+// 				canStart = true
+// 				if shouldStart {
+// 					miner.SetEtherbase(miner.coinbase)
+// 					miner.worker.start()
+// 				}
+// 			case downloader.DoneEvent:
+// 				canStart = true
+// 				if shouldStart {
+// 					miner.SetEtherbase(miner.coinbase)
+// 					miner.worker.start()
+// 				}
+// 				// Stop reacting to downloader events
+// 				events.Unsubscribe()
+// 			}
+// 		case addr := <-miner.startCh:
+// 			miner.SetEtherbase(addr)
+// 			if canStart {
+// 				miner.worker.start()
+// 			}
+// 			shouldStart = true
+// 		case <-miner.stopCh:
+// 			shouldStart = false
+// 			miner.worker.stop()
+// 		case <-miner.exitCh:
+// 			miner.worker.close()
+// 			return
+// 		}
+// 	}
+// }
 
 func (miner *Miner) Start(coinbase common.Address) {
-	miner.startCh <- coinbase
+	miner.worker.start()
 }
 
 func (miner *Miner) Stop() {
-	miner.stopCh <- struct{}{}
-}
-
-func (miner *Miner) Close() {
-	close(miner.exitCh)
+	miner.worker.stop()
 }
 
 func (miner *Miner) Mining() bool {
@@ -161,19 +154,19 @@ func (miner *Miner) Mining() bool {
 }
 
 func (miner *Miner) Hashrate() uint64 {
-	if pow, ok := miner.engine.(consensus.PoW); ok {
-		return uint64(pow.Hashrate())
-	}
+	// if pow, ok := miner.engine.(consensus.PoW); ok {
+	// 	return uint64(pow.Hashrate())
+	// }
 	return 0
 }
 
-func (miner *Miner) SetExtra(extra []byte) error {
-	if uint64(len(extra)) > params.MaximumExtraDataSize {
-		return fmt.Errorf("extra exceeds max length. %d > %v", len(extra), params.MaximumExtraDataSize)
-	}
-	miner.worker.setExtra(extra)
-	return nil
-}
+// func (miner *Miner) SetExtra(extra []byte) error {
+// 	if uint64(len(extra)) > params.MaximumExtraDataSize {
+// 		return fmt.Errorf("extra exceeds max length. %d > %v", len(extra), params.MaximumExtraDataSize)
+// 	}
+// 	miner.worker.setExtra(extra)
+// 	return nil
+// }
 
 // SetRecommitInterval sets the interval for sealing work resubmitting.
 func (miner *Miner) SetRecommitInterval(interval time.Duration) {
@@ -195,7 +188,7 @@ func (miner *Miner) SetRecommitInterval(interval time.Duration) {
 // }
 
 func (miner *Miner) SetEtherbase(addr common.Address) {
-	miner.coinbase = addr
+	// miner.coinbase = addr
 	miner.worker.setEtherbase(addr)
 }
 
