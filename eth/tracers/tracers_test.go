c1345bbbeda9283717edc8d4876f9c5f3146865e
Billy Pham <visopsys@users.noreply.github.com>
2021-06-03 09:19:14 -0700

    Replace geth tests by dcore/tests

--- eth/tracers/tracers_test.go
+++ eth/tracers/tracers_test.go
@@ -32,12 +32,12 @@ import (
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/ethereum/go-ethereum/tests"
 	"github.com/sisu-network/dcore/core"
 	"github.com/sisu-network/dcore/core/rawdb"
 	"github.com/sisu-network/dcore/core/types"
 	"github.com/sisu-network/dcore/core/vm"
 	"github.com/sisu-network/dcore/params"
+	"github.com/sisu-network/dcore/tests"
 )
 
 // To generate a new callTracer test, copy paste the makeTest method below into
